cc2dc05a703dc86fe0bc2fbaa3c41c55c7fd3bc6
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-09-07 13:02:06 +0200

    Modul004

---
+++ CSharp_Grundlagenkurs/Modul004Demo/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Modul004Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            #region Einfache Methode + Überladen von Methoden
+            //Erstes Beispiel - Methodenaufruf
+            double summe = Addiere(3, 5);
+            Console.WriteLine(summe);
+
+            //Zweites Beispiel - Überladene Methode von Addiere
+            double summe2 = Addiere(4.5, 5.5);
+            int ergebnis = Addiere(2, 4, 6, 8);
+            #endregion
+
+            #region Params
+            summe2 = BildeSumme(1, 2, 3, 4, 5, 6, 7, 3, 4, 23, 23);
+            #endregion
+
+            #region Optionale Parameter
+            ergebnis = Subtrahiere(10, 3, 2, 1);
+            ergebnis = Subtrahiere(10, 3, 2);
+            ergebnis = Subtrahiere(10, 3);
+
+            #endregion
+
+
+        }
+
+        //Jede Funktion/Methode besteht aus einem Funktions-Kopf und Funktions-Körper
+
+        //Funktion kann einen Rückgabewert haben, und kann Funktions/Methoden - Parameter verwenden
+
+        //SYNTAX Returntyp Addiere (Parameterliste) 
+
+
+        public static int Addiere (int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Addiere(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        //Variante, wie man bestehende Methode verwendet und zusätzliche Logik stellt
+        public static int Addiere(int a, int b, int c, int d)
+        {
+            return Addiere(a, b, c) + d;
+        }
+
+        //Überladen einer Methode
+        public static double Addiere (double a, double b)
+        {
+            return a + b;
+        }
+
+        public static void VersionsInformation()
+        {
+            Console.WriteLine("Version 1.2");
+        }
+
+
+        static int BildeSumme(params int[] summanden)
+        {
+            int summe = 0;
+
+            foreach (int summand in summanden)
+                summe += summand;
+
+            return summe;
+        }
+
+        static int Subtrahiere(int a, int b, int c = 0, int d = 0)
+        {
+            return a - b - c - d;
+        }
+    }
+}
---
+++ CSharp_Grundlagenkurs/Modul004bDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Modul004bDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Was sind Wertetype?
+            //> bool, int, byte, short, long, float, decimal, double, struct
+
+
+            //Was ist ein Referenztyp?
+            //string, Klassen, Interface...
+
+            int alterPersonA = 33;
+            int alterPersonB = 33;
+
+            PersonAltertUmEinJahrA(alterPersonA);
+            PersonAltertUmEinJahrB(ref alterPersonB);
+
+            Console.WriteLine(alterPersonA);
+            Console.WriteLine(alterPersonB);
+        }
+
+        static void PersonAltertUmEinJahrA(int age)
+        {
+            age++;
+        }
+
+        static void PersonAltertUmEinJahrB(ref int age)
+        {
+            age++;
+        }
+    }
+}
