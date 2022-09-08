a8c4adbf85f8ce0b4770b152790440d86fa8e57f
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-09-08 11:27:45 +0200

    OOP

--- CSharp_Grundlagenkurs/Lab005_Fahrzeuge_Loesung/Program.cs
+++ CSharp_Grundlagenkurs/Lab005_Fahrzeuge_Loesung/Program.cs
@@ -7,4 +7,31 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+
+    public class Fahrzeug
+    {
+        #region Lab 06: Properties, Methoden, Konstruktor
+
+        //Properties
+        public string Name { get; set; }
+        public int MaxGeschwindigkeit { get; set; }
+        public int AktGeschwindigkeit { get; set; }
+        public double Preis { get; set; }
+        public bool MotorLäuft { get; set; }
+
+        //Konstruktor mit Übergabeparametern und Standartwerten
+        public Fahrzeug(string name, int maxG, double preis)
+        {
+            this.Name = name;
+            this.MaxGeschwindigkeit = maxG;
+            this.Preis = preis;
+
+
+            this.AktGeschwindigkeit = 0;
+            this.MotorLäuft = false;
+        }
+
+        #endregion
+    }
 }
