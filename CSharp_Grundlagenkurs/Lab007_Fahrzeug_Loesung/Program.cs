d9a8763cb3f97de227fae673280a0ac897be8cec
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-09-08 13:35:51 +0200

    VererbungsLab

---
+++ CSharp_Grundlagenkurs/Lab007_Fahrzeug_Loesung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab007_Fahrzeug_Loesung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
