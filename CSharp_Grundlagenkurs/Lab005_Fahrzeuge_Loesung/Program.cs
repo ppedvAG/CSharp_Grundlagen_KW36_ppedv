d342234aa4cb419789809f1be398a8ae1eb77770
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-09-08 11:42:00 +0200

    Teilergebnis

--- CSharp_Grundlagenkurs/Lab005_Fahrzeuge_Loesung/Program.cs
+++ CSharp_Grundlagenkurs/Lab005_Fahrzeuge_Loesung/Program.cs
@@ -26,10 +26,27 @@
             this.Name = name;
             this.MaxGeschwindigkeit = maxG;
             this.Preis = preis;
-
-
             this.AktGeschwindigkeit = 0;
             this.MotorLäuft = false;
+        }
+
+        //Methode zum Beschleunigen und Bremsen
+        public void Beschleunige(int a)
+        {
+            //Beschleunigt werden kann nur bei einem Motor der läuft
+            if (this.MotorLäuft)
+            {
+                //Prüfung dass die Zielbeschleunigung nicht der MaxGeschwindigkeit übersteigt. 
+                //Wenn Zielbeschleunigung höher als MaxGeschwindigkeit, wird MaxGeschwinigkeit der AktGeschwindigkeit zugeordnet
+                if (this.AktGeschwindigkeit + a > this.MaxGeschwindigkeit)
+                    this.AktGeschwindigkeit = this.MaxGeschwindigkeit;
+                else if (this.AktGeschwindigkeit + a < 0) // Dieser Fall ist eher selten, aber es wäre auch Möglich beim Beschleunigen einen negativen Wert zu übergeben
+                    this.AktGeschwindigkeit = 0;
+                else
+                    this.AktGeschwindigkeit += a;
+
+                Console.WriteLine($"{this.Name} bewegt sich jetzt mit {this.AktGeschwindigkeit}km/h");
+            }
         }
 
         #endregion
